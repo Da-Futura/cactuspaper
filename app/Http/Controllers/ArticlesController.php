97b2d4abc2da4849c64d0870fc8ed37a0cbb66e3
Jean-Le <jean-le@outlook.com>
2016-11-27 15:40:18 -0400

    Switched to SSH

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -10,7 +10,7 @@ use App\Article;
 class ArticlesController extends Controller
 {
     // Queries the database for all articles and then passes an array of that
-    // to the articles/index view.
+    // to the articles.index view.
     public function index(){
         $articles = Article::all();
 
