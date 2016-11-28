09098005b883b4cb03b2e0ac176f840e808330a4
Jean-Le <jean-le@outlook.com>
2016-11-28 17:24:28 -0400

    Tested Group -- Membership relationship on the groups.show view.

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -14,7 +14,7 @@ class GroupsController extends Controller
     // Finds the Group associated with the id
     // Calls the group/show view
     public function show(Group $group){
-        $group->load('articles');
+        $group->load('articles','memberships.user');
         return view('groups.show', compact('group'));
     }
 }
--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -3,10 +3,18 @@
 @section('content')
     <h1>{{$group->name}}</h1>
 
+    <h2>Articles are</h2>
     <ul>
     @foreach($group->articles as $article)
         <li>{{$article->title}}</li>
     @endforeach
+    </ul>
+
+    <h2>Group Members are</h2>
+    <ul>
+        @foreach($group->memberships as $membership)
+            <li>{{$membership->user->name}}  {{$membership->user_role}}</li>
+        @endforeach
     </ul>
 
 @stop
