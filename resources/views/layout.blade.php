02e1d66d0a095008848403aa2d32205cc0aec1f5
Jean-Le <jean-le@outlook.com>
2016-12-01 17:21:49 -0400

    got the js in resources/js to compile into public/js and then load.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>Laravel</title>
-      <link href="{{ elixir('css/app.css') }}" rel="stylesheet"/>
+    <title>Cactuspaper</title>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet"/>
+
   </head>
   <body>
       <div class="container">
@@ -19,5 +21,6 @@
           </div>
 
       </div>
+      <script src="{{ elixir('js/app.js')}}"></script>
   </body>
 </html>
