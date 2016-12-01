5dbf41a22bb76f982af84ff60cf27a04a8375f38
Jean-Le <jean-le@outlook.com>
2016-11-30 22:20:20 -0400

    getRelArticles can return a collection of related articles and their corresponding relevence given a concept id

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -41,10 +41,11 @@ class ArticlesController extends Controller
     public function explore(Article $article){
         $article->load('conceptRelationships');
         $concept_id = $article->conceptRelationships[0]['concept_id'];
-        $relArticleIds = $this->getRelArticles($concept_id);
-        $relatedArticles = Article::findMany($relArticleIds);
+
+        return $relArticles = $this->getRelArticles($concept_id);
 
         $relatedArticleTitles = $this->getRelArticleTitles($relatedArticles);
+        return response()->json(["concept name" =>["titles" => $relatedArticleTitles]]);
 
         return $relatedArticleTitles;
 
@@ -62,17 +63,38 @@ class ArticlesController extends Controller
         return $articleTitles;
     }
 
-    // Returns an array or relative article IDs given the concept id
+
+
+    // Returns an array or relative Articles given the concept id
+    // Is of the form:
+    /*
+      {"Mars":{"articles":[{"0.95":{"id":10,"title":"A sightseer\u2019s guide to Mars","url":"http:\/\/www.bbc.com\/future\/story\/20161104-a-sightseers-guide-to-mars","author":"Zaria Gorvett","summary":"mars","user_id":1,"group_id":1,"created_at":"2016-11-30 14:25:33","updated_at":"2016-11-30 14:25:33"}},{"0.98":{"id":11,"title":"Mars probe returns first pictures - BBC News","url":"http:\/\/www.bbc.com\/news\/science-environment-38147682","author":"BBC News","summary":"More Mars","user_id":1,"group_id":1,"created_at":"2016-11-30 17:11:36","updated_at":"2016-11-30 17:11:36"}}]}} */
+
     function getRelArticles($conceptId){
         $concept = Concept::find($conceptId);
         $relationships = $concept->conceptRelationships;
 
-        $relArticleArray = [];
+        $relArticleArray = []; // returns an array of related article IDs
+        $relRelevanceArray = [];
         foreach($relationships as $relationship){
-            array_push($relArticleArray, $relationship["article_id"]);
+            array_push($relArticleArray,  $relationship["article_id"]);
+            array_push($relRelevanceArray, $relationship->relevance);
         }
 
-        return $relArticleArray;
+        $relatedArticles = Article::findMany($relArticleArray);
+
+        $articleWithSentiment = array();
+
+        $arrayLength = count($relRelevanceArray);
+
+        for($i = 0; $i <$arrayLength; $i++){
+            $newPair = [$relRelevanceArray[$i] => $relatedArticles[$i]];
+            array_push($articleWithSentiment, $newPair);
+        }
+
+        $conceptName = $concept->name;
+        return [$conceptName => ["articles" => $articleWithSentiment]];
+
     }
 
 }
