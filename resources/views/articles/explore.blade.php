72a4e45535102b80fe765c13c3257937298b832b
Jean-Le <jean-le@outlook.com>
2016-12-02 10:19:13 -0400

    did it, except for repeats

--- resources/views/articles/explore.blade.php
+++ resources/views/articles/explore.blade.php
@@ -2,15 +2,43 @@
 @section('content')
     <h1>{{$article->title}}</h1>
 
-    {{$conceptRelationshipArray[0][0]->article->title}}
+    <!-- @foreach($conceptRelationshipArray as $x)
+         @foreach($x as $y)
+         {{$x[0]->concept->name}}
+         {{$y->article->group->name}}
+         @endforeach
+         @endforeach -->
 
-    @foreach($conceptRelationshipArray as $x)
-        {{$x[0]->concept->name}}
-        @foreach($x as $y)
-            {{$y->article->title}}
-        @endforeach
-    @endforeach
+    <table>
+        <thead>
+            <tr>
+                <th>Concept</th>
+                <th>Article</th>
+                <th>Group</th>
+            </tr>
+        </thead>
 
+        <tbody>
+            @foreach($conceptRelationshipArray as $a)
+
+
+                @foreach($a as $b)
+                    @if($b->article->id != $article->id)
+                    <tr>
+                        <td>{{$a[0]->concept->name}}</td>
+                        <td>{{$b->article->title}}</td>
+                        <td>{{$b->article->group->name}}</td>
+                    </tr>
+                    @endif
+                @endforeach
+
+            @endforeach
+
+
+        </tbody>
+
+
+    </table>
 
 
 @stop
