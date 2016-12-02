1ce887cdf153ce7954e6d791f9607956c3b2401f
Jean-Le <jean-le@outlook.com>
2016-12-01 21:17:37 -0400

    Made the footer!

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -20,6 +20,7 @@
 
       @yield('content')
 
+      @include('partials.footer')
       <script src="{{ elixir('js/app.js')}}"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
   </body>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="page-footer">
+<!--    <div class="container">
+         <div class="row">
+             <div class="col l6 s12">
+             <h5>Footer Content</h5>
+             </div>
+             <div class="col l4 offset-l2 s12">
+             <h5>Links</h5>
+             <ul>
+             <li><a  href="#!">Link 1</a></li>
+             </ul>
+             </div>
+             </div>
+    </div> -->
+    <div class="footer-copyright">
+        <div class="container center">
+            © 2016 Jean-Le Des Etages
+        </div>
+    </div>
+</footer>
