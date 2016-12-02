2b38b814df4dd576c868d8fd948a9b2e5943de67
Jean-Le <jean-le@outlook.com>
2016-12-01 20:08:29 -0400

    Created registration form in materialize and it is pretty

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,11 +3,60 @@
 @section('content')
 
     @include('partials.authNav')
-    <div class="row registerRow">
+    <div class="container">
+        <div class="row registerRow">
+            <div class="col s12 m5 regCarlaCol">
+                <img alt="Carla The Cactus" src="{{asset('img/cutiecactus.png')}}" width="400px"/>
+            </div>
 
+            <div class="col s12 m6 offset-m1">
+                <form class="card" role="form" method="POST" action="{{ url('/register') }}" >
 
+                    {{ csrf_field() }}
 
-    </div> <!-- End of registerRow -->
+                    <div class="card-content">
+                        <h3>Register</h3>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="name" type="text" name="name" class="validate">
+                                <label for="name">Name</label>
+                            </div>
+                        </div>
 
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="email" type="email" name="email" class="validate">
+                                <label for="email">Email</label>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="password" type="password" class="validate" name="password">
+                                <label for="password">Password</label>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="password-confirm" type="password" name="password_confirmation" class="validate">
+                                <label for="password-confirm">Confirm Password</label>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <button class="btn waves-effect waves-light" type="submit" name="action">Register
+                                <i class="material-icons right">send</i>
+                            </button>
+                        </div>
+
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div> <!-- End of registerRow -->
+    </div>
 
 @endsection
--- resources/views/partials/authNav.blade.php
+++ resources/views/partials/authNav.blade.php
@@ -1,1 +1,14 @@
-Hello from auth nav
+<nav>
+    <div class="nav-wrapper">
+        <div class="container">
+
+            <a href="#!" class="brand-logo">CACTUSPAPER</a>
+
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li>Get ready to explore the universe</li>
+            </ul>
+
+        </div>
+
+    </div>
+</nav>
